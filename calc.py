19b66427fa21ee92a8f8ec243ad0db19ce6038f0
joaogabrielz <zacarias1021@gmail.com>
2021-11-27 15:55:58 -0400

    alteracoes nao completas

--- calc.py
+++ calc.py
@@ -6,4 +6,7 @@ option = int(input("opção: "))
 if (option == 1):
     n1 = int(input("Digite 1* numero: "))
     n2 = int(input("Digite 1* numero: "))
-    breakpoint
+    result = n1+n2
+    msg = 'result:' + str(result)
+    print (msg)
+input("press Enter..")
