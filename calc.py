99667fa494d0284b8402fd30440ee6c07146378c
joaogabrielz <zacarias1021@gmail.com>
2021-11-27 15:43:37 -0400

    primeiro commit so pra registarar

---
+++ calc.py
@@ -0,0 +1,9 @@
+#while option != 1:
+print('selecione')
+print('1-calular')
+print('2-sair')
+option = int(input("opção: "))
+if (option == 1):
+    n1 = int(input("Digite 1* numero: "))
+    n2 = int(input("Digite 1* numero: "))
+    breakpoint
